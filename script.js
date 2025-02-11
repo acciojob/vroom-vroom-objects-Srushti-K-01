5770f63a91b45f6e87b71d4adef87a8baa5fc878
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-02-11 11:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ function Car(make, model) {
 	this.make = make;
 	this.model = model;
 }
+Car.prototype.getMakeModel = function(){
+	return `${this.make} ${this.model}`;
+}
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
 	this.topSpeed = topSpeed;
 }
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed;
+}
+
 
 // class Car {
 //   constructor(make, model) {
 //     this.make = make;
 //     this.model = model;
 //   }
-// //	//Car.prototype.getMakeModel = function(){}
+// // Car.prototype.getMakeModel = function(){}
 //   getMakeModel() {
 //     return ${this.make} ${this.model};
 //   }
